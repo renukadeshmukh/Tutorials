800781d14fbcccf781c8e76fb037408aed0aa930
renukadeshmukh <renud1988@gmail.com>
2015-03-06 02:02:04 -0500

    Stack Tutorial

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(10);
+            Console.WriteLine(s.Peek());
+            s.Push(9);
+            Console.WriteLine(s.Peek());
+            s.Push(8);
+            Console.WriteLine(s.Peek());
+
+            s.Pop();
+            Console.WriteLine(s.Peek());
+            s.Pop();
+            Console.WriteLine(s.Peek());
+            s.Pop();
+            Console.WriteLine(s.Peek());
+            s.Pop();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack
+    {
+        Node Top { get; set; }
+
+        public void Push(int value) {
+            Node n = new Node(value);
+            if (Top == null)
+                Top = n;
+            else
+            {
+                n.Next = Top;
+                Top = n;
+            }
+        }
+
+        public Node Pop()
+        {
+            Node n = Top;
+            if (IsEmpty())
+                Console.WriteLine("Stack is empty !");
+            else
+            {
+                Top = Top.Next;
+            }
+            return Top;
+        }
+
+        public int Peek()
+        {
+            int topVal = -1;
+            if (IsEmpty())
+                Console.WriteLine("Stack is empty !");
+            else topVal = Top.Value;
+            return topVal;
+        }
+
+        public bool IsEmpty()
+        {
+            if (Top == null)
+                return true;
+            else return false;
+        }
+    }
+}
