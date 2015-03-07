a381b326730737ac9c0b45c570f7ce4a6f885c48
renukadeshmukh <renud1988@gmail.com>
2015-03-07 15:11:25 -0500

    Two stacks in an array
    Implemented two stack using one Array

---
+++ StackTutorials/TwoStacksInAnArray/TwoStacksInAnArray/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoStacksInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoStacks ts = new TwoStacks();
+            int n1 = ts.Pop1();
+            int n2 = ts.Pop2();
+
+            ts.Push1(1);
+            ts.DisplayArray();
+            ts.Push1(2);
+            ts.DisplayArray();
+
+            ts.Push2(8);
+            ts.DisplayArray();
+            ts.Push2(9);
+            ts.DisplayArray();
+
+            ts.Push1(3);
+            ts.DisplayArray();
+            ts.Push2(7);
+            ts.DisplayArray();
+
+            ts.Push1(4);
+            ts.DisplayArray();
+            ts.Push1(5);
+            ts.DisplayArray();
+            ts.Push1(6);
+            ts.DisplayArray();
+
+            ts.Push1(10);
+            ts.DisplayArray();
+            ts.Push1(11);
+            ts.DisplayArray();
+            ts.Push2(12);
+            ts.DisplayArray();
+
+
+            Console.WriteLine(ts.Pop1());
+            ts.DisplayArray();
+            Console.WriteLine(ts.Pop1());
+            ts.DisplayArray();
+            Console.WriteLine(ts.Pop2());
+            ts.DisplayArray();
+            Console.WriteLine(ts.Pop2());
+            ts.DisplayArray();
+            Console.WriteLine(ts.Pop2());
+            ts.DisplayArray();
+            Console.WriteLine(ts.Pop2());
+            ts.DisplayArray();
+            Console.WriteLine(ts.Pop2());
+            ts.DisplayArray();
+
+            ts.Push2(12);
+            ts.DisplayArray();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StackTutorials/TwoStacksInAnArray/TwoStacksInAnArray/TwoStacks.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoStacksInAnArray
+{
+    public class TwoStacks
+    {
+        int[] StackArr = new int[10];
+        int Top1 = -1;
+        int Top2 = -1;
+
+        public void Push1(int x)
+        {
+            if (Top1 == -1)
+            {
+                StackArr[0] = x;
+                Top1 = 0;
+            }
+            else if (Top2 - Top1 == 1 || Top1 == 9)
+                Console.WriteLine("Stack1 Full !!");
+            else
+                StackArr[++Top1] = x;
+        }
+
+        public void Push2(int x)
+        {
+            if (Top2 == -1)
+            {
+                StackArr[9] = x;
+                Top2 = 9;
+            }
+            else if (Top2 - Top1 == 1 || Top2 == 0)
+                Console.WriteLine("Stack2 Full !!");
+            else
+                StackArr[--Top2] = x;
+        }
+
+        public int Pop1()
+        {
+            if (Top1 == -1)
+                Console.WriteLine("Stack1 Empty !!");
+            else if (Top1 == 0)
+            {
+                Top1 = -1;
+                int x = StackArr[0];
+                StackArr[0] = 0;
+                return x;
+            }
+            else
+            {
+                int x = StackArr[Top1];
+                StackArr[Top1] = 0;
+                Top1--;
+                return x;
+            }
+            return -1;
+        }
+
+        public int Pop2()
+        {
+            if (Top2 == -1)
+                Console.WriteLine("Stack2 Empty !!");
+            else if (Top2 == 9)
+            {
+                int x = StackArr[9];
+                Top2 = -1;
+                StackArr[9] = 0;
+                return x;
+            }
+            else
+            {
+                int x = StackArr[Top2];
+                StackArr[Top2] = 0;
+                Top2++;
+                return x;
+            }
+            return -1;
+        }
+
+        public void DisplayArray() {
+            Console.Write("Array is :: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(StackArr[i] + " ");
+            }
+            Console.WriteLine(" ");
+        }
+       
+    }
+}
