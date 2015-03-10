cf53061568517e45691fb13a942932d59474756f
renukadeshmukh <renud1988@gmail.com>
2015-03-09 22:49:45 -0400

    Implement Queue using Array

---
+++ QueueTutorials/QueueUsingArray/QueueUsingArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue(4);
+            q.Display();
+            q.Enqueue(5);
+            q.Display();
+            q.Enqueue(6);
+            q.Display();
+            q.Enqueue(7);
+            q.Display();
+            q.Enqueue(8);
+            q.Display();
+            q.Enqueue(9);
+            q.Display();
+
+            Console.WriteLine("\nDequeuing :: ",q.Dequeue());
+            q.Display();
+            Console.WriteLine("\nDequeuing :: ", q.Dequeue());
+            q.Display();
+            Console.WriteLine("\nDequeuing :: ", q.Dequeue());
+            q.Display();
+            Console.WriteLine("\nDequeuing :: ", q.Dequeue());
+            q.Display();
+            Console.WriteLine("\nDequeuing :: ", q.Dequeue());
+            q.Display();
+            Console.WriteLine("\nDequeuing :: ", q.Dequeue());
+            q.Display();
+            Console.WriteLine("\nDequeuing :: ", q.Dequeue());
+            q.Display();
+            Console.WriteLine("\nDequeuing :: ", q.Dequeue());
+            q.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QueueTutorials/QueueUsingArray/QueueUsingArray/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingArray
+{
+    public class Queue
+    {
+        int Max = 5;
+        int[] Array = { 0, 0, 0, 0, 0 };
+        int Start;
+        int End;
+        int QueueSize = 0;
+
+        public void Enqueue(int element)
+        {
+            if (!IsFull())
+            {
+                QueueSize++;
+                End = (End + 1) % Max;
+                Array[End] = element;
+
+            }
+            else Console.WriteLine("\nQueue is Full !!!! Cannot insert {0}", element);
+        }
+
+        public int Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                QueueSize--;
+                int val = Array[Start];
+                Array[Start] = -1;
+                Start = (Start + 1) % Max;
+                return val;
+            }
+            else return -1;
+        }
+
+        public bool IsEmpty()
+        {
+            if (QueueSize == 0)
+                return true;
+            return false;
+        }
+
+        public bool IsFull()
+        {
+            if (QueueSize == Array.Length)
+                return true;
+            return false;
+        }
+
+        public void Display()
+        {
+            
+            Console.Write("\nQueue is :: ");
+            for (int i = 0; i < Max; i++)
+            {
+                Console.Write(Array[i] + " ");
+            }
+        }
+    }
+}
