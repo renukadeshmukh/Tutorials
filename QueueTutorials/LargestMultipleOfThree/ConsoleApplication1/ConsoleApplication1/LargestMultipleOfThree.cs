41dfdd4ef393cd787a40c0f305687577dbdd5636
renukadeshmukh <renud1988@gmail.com>
2015-03-13 17:38:58 -0400

    LargestMultipleOfThree
    Using List.

---
+++ QueueTutorials/LargestMultipleOfThree/ConsoleApplication1/ConsoleApplication1/LargestMultipleOfThree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class LargestMultipleOfThree
+    {
+        public string GetLargestNumber(List<int> array) {
+            List<int> q0 = new List<int>();
+            List<int> q1 = new List<int>();
+            List<int> q2 = new List<int>();
+
+            foreach (var item in array)
+            {
+                if (item % 3 == 0)
+                    q0.Add(item);
+                else if (item % 3 == 1)
+                    q1.Add(item);
+                else q2.Add(item);
+
+            }
+            q0.Sort((a, b) => -1 * a.CompareTo(b)); 
+            q1.Sort((a, b) => -1 * a.CompareTo(b));
+            q2.Sort((a, b) => -1 * a.CompareTo(b)); 
+
+            while (true)
+            {
+
+                if (q1.Count>0 && q2.Count>0)
+                {
+                    int e1 = q1[0];
+                    int e2 = q2[0];
+                    q0.Add(e1);
+                    q0.Add(e2);
+                   q1.RemoveAt(0);
+                    q2.RemoveAt(0);
+                }
+                else break;
+            }
+
+            q0.Sort((a, b) => -1 * a.CompareTo(b)); 
+            
+            string output = string.Empty;
+            for (int i = 0; i <q0.Count; i++)
+            {
+                output = output+ q0[i] + " ";
+            }
+
+            return output.Trim();
+
+        }
+    }
+}
---
+++ QueueTutorials/LargestMultipleOfThree/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LargestMultipleOfThree lmt = new LargestMultipleOfThree();
+            List<int> list = new List<int> { 8, 1, 7, 6, 0 };
+            string s = lmt.GetLargestNumber(list);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
